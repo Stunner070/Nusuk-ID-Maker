189a72b57ebed10f20833c94430421c2fc045539
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2024-05-31 19:25:02 +0200

    foundation of application

---
+++ src/commons/PDFReader.java
@@ -0,0 +1,38 @@
+package commons;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.openqa.selenium.devtools.v123.domsnapshot.model.StringIndex;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class PDFReader {
+
+    private final PDFExtractor pdfExtractor;
+    private final PDFFinder pdfFinder;
+
+    public PDFReader() {
+        this.pdfExtractor = new PDFExtractor();
+        this.pdfFinder = new PDFFinder();
+    }
+
+    public List<String> pdfReader(String filePath) throws IOException {
+        List<String> info = null;
+        try {
+            File file = pdfFinder.findPDF(filePath);
+            PDDocument document = Loader.loadPDF(file);
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+
+            info = pdfExtractor.extractInfo(text);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return info;
+    }
+
+}
