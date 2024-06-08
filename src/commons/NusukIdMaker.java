48454a1e29a32d97e7858704b5e357ff329a3c9f
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2024-06-08 20:25:37 +0200

    refactoring and added checkstyle

---
+++ src/commons/NusukIdMaker.java
@@ -0,0 +1,19 @@
+package commons;
+
+import java.io.IOException;
+
+public class NusukIdMaker {
+
+    public static void main(String[] args) throws IOException {
+        PDFReader pdfReader = new PDFReader();
+        System.out.println(pdfReader.pdfReader("C:\\Users\\sofya\\Downloads\\rana.pdf"));
+    }
+
+
+
+
+
+
+
+
+}
