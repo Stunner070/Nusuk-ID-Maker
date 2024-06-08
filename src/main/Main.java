c1ce9948602bb5d0b09efe1b16c58992cf6c8bd9
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2024-06-08 21:39:16 +0200

    small change in fxml name

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var frontPage = FXML.load(FrontPageController.class, "scenes/test.fxml");
+        var frontPage = FXML.load(FrontPageController.class, "scenes/frontPage.fxml");
 
         var mainctrl = injector.getInstance(MainCtrl.class);
         mainctrl.initialize(primaryStage, frontPage);
